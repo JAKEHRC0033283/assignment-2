2457fc3ef1220a86c334df0f7f378f485f458769
Holloway <rc0033283@student.riversidecollege.ac.uk>
2019-10-08 12:24:55 +0100

    Add project files.

---
+++ assignment 2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            double r1, r2;
+
+            try 
+        }
+        {
+                sr1 = double.Parse(textBox1.Text)
+                    }
+            catch
+            {
+
+            }
